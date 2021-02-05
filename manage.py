4c82c507a44181ac07869ba504fe6a2e0c50b728
BuildTools <unconfigured@null.spigotmc.org>
2021-02-05 23:16:37 +0300

    Demo

--- manage.py
+++ manage.py
@@ -2,9 +2,6 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test.settings")
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+a = 0
+while True:
+    a = a + 1
